ca27745d59e1466d947d1bf1a3fb08169f511f5c
Ahmedssamir <ahmedsamir1994@gmail.com>
2023-04-04 18:18:31 +0200

    mahmoud samir

---
+++ resources/views/layouts/main-sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="container-fluid">
+    <div class="row">
+        <!-- Left Sidebar start-->
+        <div class="side-menu-fixed">
+            <div class="scrollbar side-menu-bg">
+                <ul class="nav navbar-nav side-menu" id="sidebarnav">
+
+                    <!-- menu title -->
+                    <li class="mt-10 mb-10 text-muted pl-4 font-medium menu-title">الرئيسيه </li>
+                    <!-- menu item Elements-->
+                    <li>
+                        <a href="javascript:void(0);" data-toggle="collapse" data-target="#elements">
+                            <div class="pull-left"><i class="ti-palette"></i><span
+                                    class="right-nav-text">الاعدادات العامة</span></div>
+                            <div class="pull-right"><i class="ti-plus"></i></div>
+                            <div class="clearfix"></div>
+                        </a>
+                        <ul id="elements" class="collapse" data-parent="#sidebarnav">
+                            <li><a href="{{route('slider')}}">slider</a></li>
+                            <li><a href="{{route('Portfolio')}}">Portfolio</a></li>
+                            <li><a href="{{route('Price')}}">Price</a></li>
+                            <li><a href="{{route('client')}}">client</a></li>
+                            <li><a href="{{route('team')}}">team</a></li>
+                            <li><a href="{{route('artical')}}">artical</a></li>
+                            <li><a href="{{route('setting')}}">CompanySetting</a></li>
+
+
+                        </ul>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+        </div>
+        </div>
+
+        <!-- Left Sidebar End-->
+
+        <!--=================================
